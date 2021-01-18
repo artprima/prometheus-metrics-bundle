b176e4281fd72ee1b1146ff935cb64e93ac0a290
Denis Voytyuk <denisvmedia@users.noreply.github.com>
2021-01-18 16:52:03 +0100

    Fix integration test

--- .integration/symfony/src/Controller/DummyController.php
+++ .integration/symfony/src/Controller/DummyController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
