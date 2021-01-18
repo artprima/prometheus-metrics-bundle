64b821586c7b785dbc8021865cf162b77ab3355b
Denis Voytyuk <denisvmedia@users.noreply.github.com>
2021-01-18 15:14:59 +0100

    Add debug output prometheus metrics

--- .integration/symfony/tests/Functional/ArtprimaPrometheusBundleTest.php
+++ .integration/symfony/tests/Functional/ArtprimaPrometheusBundleTest.php
@@ -13,6 +13,7 @@ class ArtprimaPrometheusBundleTest extends WebTestCase
         $client->request('GET', '/metrics/prometheus');
         self::assertResponseIsSuccessful();
         $content = $client->getResponse()->getContent();
+        echo $content;
         $expected = "# HELP php_info Information about the PHP environment.\n# TYPE php_info gauge\nphp_info{version=\"%s\"} 1";
         self::assertContains(
             sprintf($expected, PHP_VERSION),
