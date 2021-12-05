2ae5d3f8dddfc6ba9509e9d57554fbd1e44c566b
Denis Voytyuk <denisvmedia@users.noreply.github.com>
2021-12-05 18:40:46 +0100

    Remove isMasterRequest support

--- EventListener/MetricsCollectorListener.php
+++ EventListener/MetricsCollectorListener.php
@@ -49,7 +49,7 @@ class MetricsCollectorListener implements LoggerAwareInterface
 
     public function onKernelRequestPre(RequestEvent $event): void
     {
-        if (method_exists($event, 'isMainRequest') ? !$event->isMainRequest() : !$event->isMasterRequest()) {
+        if (!$event->isMainRequest()) {
             return;
         }
 
@@ -73,7 +73,7 @@ class MetricsCollectorListener implements LoggerAwareInterface
 
     public function onKernelRequest(RequestEvent $event): void
     {
-        if (method_exists($event, 'isMainRequest') ? !$event->isMainRequest() : !$event->isMasterRequest()) {
+        if (!$event->isMainRequest()) {
             return;
         }
 
