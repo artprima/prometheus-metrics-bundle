52ff76a160cadb3c479064181e984adacbf04a68
Sébastien <stanneux@b2pweb.com>
2022-05-25 10:12:08 +0200

    Add console command to clear metrics

---
+++ Command/ClearMetricsCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Artprima\PrometheusMetricsBundle\Command;
+
+use Prometheus\Storage\Adapter;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Clear metrics from prometheus storage
+ */
+class ClearMetricsCommand extends Command
+{
+    protected static $defaultName = 'artprima:prometheus:metrics:clear';
+
+    /**
+     * @var Adapter
+     */
+    private $storage;
+
+
+    /**
+     * @param Adapter $storage
+     */
+    public function __construct(Adapter $storage)
+    {
+        parent::__construct(static::$defaultName);
+
+        $this->storage = $storage;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Clear all collected metrics from storage')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln(sprintf('Clearing storage from <comment>%s</comment>', get_class($this->storage)));
+
+        $this->storage->wipeStorage();
+
+        $output->writeln('<success>The storage was successfully cleared.</success>');
+
+        return 0;
+    }
+}
---
+++ Tests/Command/ClearMetricsCommandTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Artprima\PrometheusMetricsBundle\Command;
+
+use Artprima\PrometheusMetricsBundle\Command\ClearMetricsCommand;
+use PHPUnit\Framework\TestCase;
+use Prometheus\Storage\Adapter;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ *
+ */
+class ClearMetricsCommandTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testClear()
+    {
+        $adapter = $this->createMock(Adapter::class);
+        $adapter->expects($this->once())->method('wipeStorage');
+        $className = get_class($adapter);
+
+        $command = new ClearMetricsCommand($adapter);
+        $application = new Application();
+        $application->add($command);
+
+        $tester = new CommandTester($application->get('artprima:prometheus:metrics:clear'));
+        $tester->execute([]);
+
+        $expected = <<<EOL
+Clearing storage from {$className}
+<success>The storage was successfully cleared.</success>
+
+EOL;
+
+        $this->assertSame($expected, $tester->getDisplay());
+    }
+
+}
