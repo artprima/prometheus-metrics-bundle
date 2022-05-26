e55eb001a83d998e2b3774f690dfa1516e62d212
Sébastien <stanneux@b2pweb.com>
2022-05-26 11:37:44 +0200

    Fix phpcs

--- Tests/StorageFactory/APCngFactoryTest.php
+++ Tests/StorageFactory/APCngFactoryTest.php
@@ -10,13 +10,14 @@ use Prometheus\Storage\APCng;
 
 class APCngFactoryTest extends TestCase
 {
-//    public function testCreate(): void
-//    {
-//        $factory = new APCngFactory();
-//
-//        self::assertInstanceOf(APCng::class, $factory->create([]));
-//    }
+    /**
+    public function testCreate(): void
+    {
+        $factory = new APCngFactory();
 
+        self::assertInstanceOf(APCng::class, $factory->create([]));
+    }
+     */
     public function testFactoryName(): void
     {
         $factory = new APCngFactory();
--- Tests/StorageFactory/ApcFactoryTest.php
+++ Tests/StorageFactory/ApcFactoryTest.php
@@ -10,13 +10,14 @@ use Prometheus\Storage\APC;
 
 class ApcFactoryTest extends TestCase
 {
-//    public function testCreate(): void
-//    {
-//        $factory = new ApcFactory();
-//
-//        self::assertInstanceOf(APC::class, $factory->create([]));
-//    }
+    /**
+    public function testCreate(): void
+    {
+        $factory = new ApcFactory();
 
+        self::assertInstanceOf(APC::class, $factory->create([]));
+    }
+     */
     public function testFactoryName(): void
     {
         $factory = new ApcFactory();
