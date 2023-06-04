8ef67bd400dd50de12d78c210da9666be096438f
Denis Voytyuk <5462781+denisvmedia@users.noreply.github.com>
2023-06-04 17:00:33 +0200

    Fix tests

--- .integration/symfony/tests/Functional/ArtprimaPrometheusBundleTest.php
+++ .integration/symfony/tests/Functional/ArtprimaPrometheusBundleTest.php
@@ -19,31 +19,31 @@ class ArtprimaPrometheusBundleTest extends WebTestCase
         $content = $client->getResponse()->getContent();
         echo $content;
         $expected = "# HELP php_info Information about the PHP environment.\n# TYPE php_info gauge\nphp_info{version=\"%s\"} 1";
-        self::assertContains(
+        self::assertStringContainsString(
             sprintf($expected, PHP_VERSION),
             trim($content)
         );
-        self::assertContains(
+        self::assertStringContainsString(
             'symfony_http_2xx_responses_total{action="GET-app_dummy_homepage"} 1'.PHP_EOL,
             $content
         );
-        self::assertContains(
+        self::assertStringContainsString(
             'symfony_http_2xx_responses_total{action="all"} 1'.PHP_EOL,
             $content
         );
-        self::assertContains(
+        self::assertStringContainsString(
             'symfony_http_requests_total{action="GET-app_dummy_homepage"} 1'.PHP_EOL,
             $content
         );
-        self::assertContains(
+        self::assertStringContainsString(
             'symfony_http_requests_total{action="all"} 1'.PHP_EOL,
             $content
         );
-        self::assertContains(
+        self::assertStringContainsString(
             'symfony_instance_name{instance="dev"} 1'.PHP_EOL,
             $content
         );
-        self::assertContains(
+        self::assertStringContainsString(
             'symfony_app_version{version="1.2.3"} 1'.PHP_EOL,
             $content
         );
@@ -60,7 +60,7 @@ class ArtprimaPrometheusBundleTest extends WebTestCase
         self::assertResponseIsSuccessful();
         $content = $client->getResponse()->getContent();
         echo $content;
-        self::assertContains(
+        self::assertStringContainsString(
             'symfony_exception{class="RuntimeException"} 1'.PHP_EOL,
             $content
         );
@@ -83,7 +83,7 @@ class ArtprimaPrometheusBundleTest extends WebTestCase
         self::assertResponseIsSuccessful();
         $content = $client->getResponse()->getContent();
         echo $content;
-        self::assertContains(
+        self::assertStringContainsString(
             'symfony_exception{class="RuntimeException"} 1'.PHP_EOL,
             $content
         );
@@ -96,7 +96,7 @@ class ArtprimaPrometheusBundleTest extends WebTestCase
         self::assertResponseIsSuccessful();
         $content = $client->getResponse()->getContent();
         echo $content;
-        self::assertNotContains(
+        self::assertStringNotContainsString(
             'symfony_exception{class="RuntimeException"} 1'.PHP_EOL,
             $content
         );
