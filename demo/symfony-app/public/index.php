64cf23cdb0a9889436cddb329e1abd8d7b1e0a9a
Florian Dorn <florian.dorn@boku.ac.at>
2026-02-12 10:14:39 +0100

    deps: use symfony 8 in demo app

--- demo/symfony-app/public/index.php
+++ demo/symfony-app/public/index.php
@@ -1,14 +1,10 @@
 <?php
 
 declare(strict_types=1);
-
 use App\Kernel;
-use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'prod', (bool) ($_SERVER['APP_DEBUG'] ?? false));
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context): Kernel {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
