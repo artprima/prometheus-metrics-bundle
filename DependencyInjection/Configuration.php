e92931c756b98bec6c620d75495f3c6326a35723
Denis V <5462781+denisvmedia@users.noreply.github.com>
2026-04-12 13:49:30 +0200

    style: fix Configuration import spacing

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Artprima\PrometheusMetricsBundle\DependencyInjection;
 
 use Prometheus\Histogram;
-
 use Symfony\Component\Config\Definition\BaseNode;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
